d943f9af296a130df3c5cc9a91f71bfab93555bb
westxave <westxave@kkg-do.eu>
2022-09-13 10:21:26 +0200

    refactor

--- src/main/java/my_project/model/User.java
+++ src/main/java/my_project/model/User.java
@@ -1,7 +1,6 @@
 package my_project.model;
 
 import KAGO_framework.model.abitur.netz.Client;
-import my_project.control.ProgramController;
 
 public class User extends Client {
 
@@ -27,7 +26,7 @@ public class User extends Client {
     public void sendMessage(String pMessage){
         String[] m = pMessage.split(" ");
 
-        if(m[0].equals("/setname")){    //Setname
+        if(m[0].equals("/setname")){
             if(m[1].matches("^[a-zA-Z]$+") && m.length == 2){
                 send("SETNAME_"+m[1]);
                 setName(m[1]);
@@ -35,13 +34,13 @@ public class User extends Client {
                 System.out.println("INVALID_NAME");
             }
 
-        }else if(m[0].equals("/join") && m.length == 1){    //Join
+        }else if(m[0].equals("/join") && m.length == 1){
             send("JOIN");
 
-        }else if(m[0].equals("/leave") && m.length == 1){    //Leave
+        }else if(m[0].equals("/leave") && m.length == 1){
             send("LEAVE");
 
-        }else if(m[0].equals("/whisper") && m.length > 2){   //Whisper
+        }else if(m[0].equals("/whisper") && m.length > 2){
             String message = "WHISPER_"+m[1]+"_";
             for(int i = 2; i < m.length; i++){
                 message += m[i] + " ";
