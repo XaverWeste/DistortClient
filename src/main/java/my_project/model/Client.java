6ed805186cde923429d34f58c9d47d0b2a577691
westxave <westxave@kkg-do.eu>
2022-09-13 10:26:07 +0200

    refactor

--- src/main/java/my_project/model/Client.java
+++ src/main/java/my_project/model/Client.java
@@ -13,10 +13,6 @@ public class Client extends KAGO_framework.model.abitur.netz.Client {
         //TODO react
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName(){
         return name;
     }
@@ -26,14 +22,14 @@ public class Client extends KAGO_framework.model.abitur.netz.Client {
         if(m[0].equals("/setname")){
             if(m[1].matches("^[a-zA-Z]$+") && m.length == 2){
                 send("SETNAME_"+m[1]);
-                setName(m[1]);
+                name=(m[1]);
             }else System.out.println("INVALID_NAME");
         }else if(m[0].equals("/join") && m.length == 1) send("JOIN");
         else if(m[0].equals("/leave") && m.length == 1) send("LEAVE");
         else if(m[0].equals("/whisper") && m.length > 2){
-            String message = "WHISPER_"+m[1]+"_";
-            for(int i = 2; i < m.length; i++) message += m[i] + " ";
-            send(message);
+            StringBuilder message = new StringBuilder("WHISPER_" + m[1] + "_");
+            for(int i = 2; i < m.length; i++) message.append(m[i]).append(" ");
+            send(message.toString());
         }else send("MESSAGE_" + pMessage);
     }
 
