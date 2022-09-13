971c59a0a0d2a5d77adedd726cb8c082cda64546
westxave <westxave@kkg-do.eu>
2022-09-13 10:33:38 +0200

    refactor

--- src/main/java/my_project/model/Client.java
+++ src/main/java/my_project/model/Client.java
@@ -13,24 +13,33 @@ public class Client extends KAGO_framework.model.abitur.netz.Client {
         //TODO react
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getName(){
         return name;
     }
 
-    public void sendMessage(String pMessage){
-        String[] m = pMessage.split(" ");
-        if(m[0].equals("/setname")){
-            if(m[1].matches("^[a-zA-Z]$+") && m.length == 2){
-                send("SETNAME_"+m[1]);
-                name=(m[1]);
-            }else System.out.println("INVALID_NAME");
-        }else if(m[0].equals("/join") && m.length == 1) send("JOIN");
-        else if(m[0].equals("/leave") && m.length == 1) send("LEAVE");
-        else if(m[0].equals("/whisper") && m.length > 2){
-            StringBuilder message = new StringBuilder("WHISPER_" + m[1] + "_");
-            for(int i = 2; i < m.length; i++) message.append(m[i]).append(" ");
-            send(message.toString());
-        }else send("MESSAGE_" + pMessage);
+    public void sendMessage(String message){
+        String[] m = message.split(" ");
+        switch(m[0]) {
+            case "/setname" -> {
+                if (m[1].matches("^[a-zA-Z]+$") && m.length == 2) {
+                    send("SETNAME_" + m[1]);
+                    setName(m[1]);
+                }else System.out.println("INVALID_NAME");
+            }
+            case "/join" -> send("JOIN");
+            case "/leave" -> send("LEAVE");
+            case "/wisper" -> {
+                if(m.length>2) {
+                    StringBuilder sb = new StringBuilder("WHISPER_" + m[1] + "_");
+                    for (int i = 2; i < m.length; i++) sb.append(m[i]).append(" ");
+                    send(sb.toString());
+                }
+            }
+            default -> send("MESSAGE_" + message);
+        }
     }
-
 }
