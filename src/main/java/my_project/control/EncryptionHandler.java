d3bc327a94f1b0767245efa68cb4db302bec7175
westxave <westxave@kkg-do.eu>
2022-09-20 10:54:08 +0200

    verschlüsselungsklassen

---
+++ src/main/java/my_project/control/EncryptionHandler.java
@@ -0,0 +1,17 @@
+package my_project.control;
+
+public class EncryptionHandler {
+
+    private static final String[] s=new String[]{};
+
+    public static String encrypt(String s){
+
+        return null;
+    }
+
+    public static String decrypt(String s){
+
+        return null;
+    }
+
+}
